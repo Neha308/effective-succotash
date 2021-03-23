6c338373ad09b05f7fc5b679e31379a80f3542b5
Neha308 <49152389+Neha308@users.noreply.github.com>
2021-03-23 17:46:15 +0530

    made changes

--- DemoProject/src/Sample.java
+++ DemoProject/src/Sample.java
@@ -3,7 +3,7 @@ public class Sample {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Helloooo");
+		System.out.println("Helloooo hioioooio");
 		System.out.println("HII");
 		System.out.println("some code");
 		
